8559305704def53e81da85d5f6cc2fd039c5b265
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-11-01 02:15:16 +0530

    Create Registration No. to EmaiID.c

---
+++ Registration No. to EmaiID.c
@@ -0,0 +1,256 @@
+#include<stdio.h>
+void main() 
+{
+	int a;
+	printf("Enter the Reg No: RA22110420100");
+	scanf("%d",&a);
+	printf("Note: Add '@srmist.edu.in' after the SrmIST_ID\n enter 0 for info\n");
+	if(a == 01)
+	{
+		printf("Name : MANI GANNAMANENI\nSrmIST_ID : mg7284"); 
+	}
+	else if(a == 02)
+	{
+	    printf("Name : CEVIN CHRISTO\nSrmIST_ID : cc3963");
+	}
+	else if(a == 03)
+	{ 
+		printf("Name : ARYAN SINGH\nSrmIST_ID : as6655");
+	}
+	else if(a == 04)
+	{ 
+		printf("Name : NEEL DELIVALA\nSrmIST_ID : nd2160");
+	}
+	else if(a == 05)
+	{ 
+		printf("Name : JAYADEEP G L\nSrmIST_ID : jg5918");
+	}
+	else if(a == 06)
+	{ 
+		printf("Name : SHARANYA SINGH\nSrmIST_ID : ss0506");
+	}
+	else if(a == 07)
+	{ 
+		printf("Name : BASTIN K.J\nSrmIST_ID : bk3516");
+	}
+	else if(a == 8)
+	{ 
+		printf("Name : SHARAN V\nSrmIST_ID : sv6151");
+	}
+	else if(a == 9)
+	{ 
+		printf("Name : YESHWIN K E\nSrmIST_ID : yk3570");
+	}
+	else if(a == 10)
+	{ 
+		printf("Name : KAVYA JHAMB\nSrmIST_ID : kj0971");
+	}
+	else if(a == 11)
+	{ 
+		printf("Name : SIDDHARTH DHADWAL\nSrmIST_ID : sd3734");
+	}
+	else if(a == 12)
+	{ 
+		printf("Name : MRIDUL MEHRA\nSrmIST_ID : mm3921");
+	}
+	else if(a == 13)
+	{ 
+		printf("Name : MANONITHILAN SUNDARAM\nSrmIST_ID : ms0479");
+	}
+	else if(a == 14)
+	{
+	    printf("Name : VISHAL RAMPRASAD\nSrmIST_ID : vr8075");
+	}
+	else if(a == 15)
+	{ 
+		printf("Name : SIDHESH MISHRA\nSrmIST_ID : sm2730");
+	}
+	else if(a == 16)
+	{ 
+		printf("Name : RITIKA VYAS\nSrmIST_ID : rv9748");
+	}
+	else if(a == 17)
+	{ 
+		printf("Name : YESHWANTH RAGHAVENDRA RAVI SHANKAR R\nSrmIST_ID : yr9786");
+	}
+	else if(a == 18)
+	{ 
+		printf("Name : ANANT JAIN\nSrmIST_ID : aj9642");
+	}
+	else if(a == 19)
+	{ 
+		printf("Name : ROEL CHRISTRY\nSrmIST_ID : rc0827");
+	}
+	else if(a == 20)
+	{
+	    printf("Name : AYUSH JAISWAL\nSrmIST_ID : aj0682");
+	}
+	else if(a == 21)
+	{ 
+		printf("Name : SIDDHU VALLURI\nSrmIST_ID : sv9865");
+	}
+	else if(a == 22)
+	{ 
+		printf("Name : PRAGYA GAUTAM\nSrmIST_ID : pg4575");
+	}
+	else if(a == 23)
+	{ 
+		printf("Name : PRATYUSH GOUTAM\nSrmIST_ID : pg8816");
+	}
+	else if(a == 24)
+	{ 
+		printf("Name : ADITHYA SAI DAYANAND\nSrmIST_ID : as3379");
+	}
+	else if(a == 25)
+	{ 
+		printf("Name : KUSHAL PATEL\nSrmIST_ID : kp9155");
+	}
+	else if(a == 26)
+	{
+	    printf("Name : SAMAY MODY\nSrmIST_ID : sm7525");
+	}
+	else if(a == 27)
+	{ 
+		printf("Name : AANKIT SATPATHY\nSrmIST_ID : as5185");
+	}
+	else if(a == 28)
+	{ 
+		printf("Name : DIVYANSHU KUMAR\nSrmIST_ID : dk4269");
+	}
+	else if(a == 29)
+	{ 
+		printf("Name : LAKSHAY SHANDIL\nSrmIST_ID : ls6042");
+	}
+	else if(a == 30)
+	{ 
+		printf("Name : MIMANSHA JHA\nSrmIST_ID : mj2680");
+	}
+	else if(a == 31)
+	{ 
+		printf("Name : \nSrmIST_ID : ");
+	}
+	else if(a == 32)
+	{ 
+		printf("Name : SIDHANSU KESHRI\nSrmIST_ID : sk7360");
+	}
+	else if(a == 33)
+	{ 
+		printf("Name : TANVI GOEL\nSrmIST_ID : tg2284");
+	}
+	else if(a == 34)
+	{ 
+		printf("Name : SARAH SAMUEL\nSrmIST_ID : ss9847");
+	}
+	else if(a == 35)
+	{ 
+		printf("Name : RAGHAVENDRA BOHRA\nSrmIST_ID : rb4867");
+	}
+	else if(a == 36)
+	{ 
+		printf("Name : LOHITHRAMAN S\nSrmIST_ID : ls5439");
+	}
+	else if(a == 37)
+	{ 
+		printf("Name : NIKHIL SHINDE\nSrmIST_ID : ns4969");
+	}
+	else if(a == 38)
+	{ 
+		printf("Name : RAJITHA ANANTHAKRISHNAN\nSrmIST_ID : ra5111");
+	}
+	else if(a == 39)
+	{ 
+		printf("Name : ALOK CHEDAMBATH\nSrmIST_ID : ac1908");
+	}
+	else if(a == 40)
+	{ 
+		printf("Name : MEDHA VASIREDDI\nSrmIST_ID : mv2674");
+	}
+	else if(a == 41)
+	{ 
+		printf("Name : ARAVETI SARVESH\nSrmIST_ID : as1003");
+	}
+	else if(a == 42)
+	{
+	    printf("Name : SANYA MATHUR\nSrmIST_ID : sm0810");
+	}
+	else if(a == 43)
+	{ 
+		printf("Name : VISHNU K\nSrmIST_ID : vk5776");
+	}
+	else if(a == 44)
+	{ 
+		printf("Name : DIVYANSHU KOCHAR\nSrmIST_ID : dk0115");
+	}
+	else if(a == 45)
+	{ 
+		printf("Name : MURALI KRISHNA DURISETY\nSrmIST_ID : md6143");
+	}
+	else if(a == 46)
+	{ 
+		printf("Name : SAI TEJA CHEBROLU\nSrmIST_ID : sc1095");
+	}
+	else if(a == 47)
+	{ 
+		printf("Name : MOHAN PADALA\nSrmIST_ID : mp4582");
+	}
+	else if(a == 48)
+	{ 
+		printf("Name : THALIBOIENA SRINIVAS\nSrmIST_ID : ts4958");
+	}
+	else if(a == 49)
+	{ 
+		printf("Name : HAIDER SHAH\nSrmIST_ID : hs7384");
+	}
+	else if(a == 50)
+	{ 
+		printf("Name : APURV BHARDWAJ\nSrmIST_ID : ab6726");
+	}
+	else if(a == 51)
+	{ 
+		printf("Name : CHERRY SHARMA\nSrmIST_ID : cs6527");
+	}
+	else if(a == 52)
+	{ 
+		printf("Name : \nSrmIST_ID : ");
+	}
+	else if(a == 53)
+	{ 
+		printf("Name : BALAKUMAR RAGURAMM\nSrmIST_ID : br5305");
+	}
+	else if(a == 54)
+	{ 
+		printf("Name : ABHISHAKE S SARAVANAKUMAR\nSrmIST_ID : as9351");
+	}
+	else if(a == 55)
+	{ 
+		printf("Name : JAYASTRI NIVEDHA\nSrmIST_ID : jn3336");
+	}
+	else if(a == 56)
+	{ 
+		printf("Name : DEVESH S\nSrmIST_ID : ds2406");
+	}
+	else if(a == 57)
+	{ 
+		printf("Name : AKSHAY KHANNA T K\nSrmIST_ID : at9629");
+	}
+	else if(a == 58)
+	{ 
+		printf("Name : \nSrmIST_ID : ");
+	}
+	else if(a == 59)
+	{ 
+		printf("Name : SANKALP ANAND\nSrmIST_ID : sa1575");
+	}
+	else if(a == 60)
+	{ 
+		printf("Name : AADITHYA ANAND\nSrmIST_ID : aa9880");
+	}
+	else if(a == 61)
+	{ 
+		printf("Name : VARENYAM DWIVEDI\nSrmIST_ID : vd1698");
+	}
+	else if(a == 00)
+	{
+	    printf("Information About Reg No. given below is \nxxxx UNAVAILABLE xxxx\nRA2211042010031\nRA2211042010052\nRA2211042010058 ");
+	}
+}
